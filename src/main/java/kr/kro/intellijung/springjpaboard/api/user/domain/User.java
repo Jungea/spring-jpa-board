01f7e594be8a6a845e59eb930360fb8309186b00
jea <jea@onlinepowers.com>
2023-06-19 17:39:32 +0900

    H2 작동 설정

--- src/main/java/kr/kro/intellijung/springjpaboard/api/user/domain/User.java
+++ src/main/java/kr/kro/intellijung/springjpaboard/api/user/domain/User.java
@@ -3,8 +3,10 @@ package kr.kro.intellijung.springjpaboard.api.user.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "MY_USER")
 public class User {
 
     @Id
