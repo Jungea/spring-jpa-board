eb57b4d542ae7ffaee28e29d35ad43cc30df71ce
Jungea <jungea2241@gmail.com>
2023-07-09 16:40:36 +0900

    패키지이름 변경

--- board-web/src/main/java/kr/kro/intellijung/board/home/HomeController.java
+++ board-web/src/main/java/kr/kro/intellijung/board/home/HomeController.java
@@ -1,4 +1,4 @@
-package kr.kro.intellijung.board.main;
+package kr.kro.intellijung.board.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
