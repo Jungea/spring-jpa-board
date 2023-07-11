872fb70619eac279f88974ae245ebab3b8a50bf4
Jungea <jungea2241@gmail.com>
2023-07-11 23:37:37 +0900

    [core spring security] 4.8 WebAuthenticationDetails

--- board-common/src/main/java/kr/kro/intellijung/board/security/configs/SecurityConfig.java
+++ board-common/src/main/java/kr/kro/intellijung/board/security/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final AuthenticationDetailsSource authenticationDetailsSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -43,6 +45,7 @@ public class SecurityConfig {
                 .formLogin(login -> login
                         .loginPage("/login")
                         .loginProcessingUrl("/login_proc")
+                        .authenticationDetailsSource(authenticationDetailsSource)
                         .defaultSuccessUrl("/")
                         .permitAll());
 
--- board-common/src/main/java/kr/kro/intellijung/board/security/provider/CustomAuthenticationProvider.java
+++ board-common/src/main/java/kr/kro/intellijung/board/security/provider/CustomAuthenticationProvider.java
@@ -1,9 +1,11 @@
 package kr.kro.intellijung.board.security.provider;
 
+import kr.kro.intellijung.board.security.common.FormWebAuthenticationDetails;
 import kr.kro.intellijung.board.security.service.AccountContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -24,12 +26,16 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         AccountContext accountContext = (AccountContext) userDetailsService.loadUserByUsername(username);
 
         if (!passwordEncoder.matches(password, accountContext.getPassword())) {
-            throw new BadCredentialsException("BadCredentialsException");
+            throw new BadCredentialsException("Invalid password");
         }
 
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails) authentication.getDetails();
+        String secretKey = details.getSecretKey();
+        if (secretKey == null || !"secret".equals(secretKey)) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
 
-        return authenticationToken;
+        return new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
     }
 
     @Override
