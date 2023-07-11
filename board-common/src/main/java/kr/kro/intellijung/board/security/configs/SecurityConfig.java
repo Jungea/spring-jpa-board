cf101713daba93a597dad70d48eb17fca34d2446
Jungea <jungea2241@gmail.com>
2023-07-11 22:44:16 +0900

    [core spring security] 4.5 CustomAuthenticationProvider

--- board-common/src/main/java/kr/kro/intellijung/board/security/configs/SecurityConfig.java
+++ board-common/src/main/java/kr/kro/intellijung/board/security/configs/SecurityConfig.java
@@ -1,11 +1,15 @@
 package kr.kro.intellijung.board.security.configs;
 
+import kr.kro.intellijung.board.security.provider.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +18,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,6 +44,13 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin(withDefaults());
 
+        http.authenticationProvider(authenticationProvider());
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider(userDetailsService, passwordEncoder());
     }
 }
