6961a802eb9584cf2a36a11ac49f19b4b497d7ca
jea <jea@onlinepowers.com>
2023-07-20 13:43:56 +0900

    로그인 후 인증 결과 저장

--- board-common/src/main/java/kr/kro/intellijung/board/security/handler/AjaxAuthenticationSuccessHandler.java
+++ board-common/src/main/java/kr/kro/intellijung/board/security/handler/AjaxAuthenticationSuccessHandler.java
@@ -8,13 +8,19 @@ import kr.kro.intellijung.board.user.domain.Account;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private ObjectMapper objectMapper = new ObjectMapper();
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -23,6 +29,13 @@ public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHa
 
         response.setStatus(HttpStatus.OK.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        // 인증 결과 저장
+        SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        context.setAuthentication(authentication);
+        securityContextHolderStrategy.setContext(SecurityContextHolder.getContext());
+        securityContextRepository.saveContext(context, request, response);
 
         objectMapper.writeValue(response.getWriter(), account);
 
